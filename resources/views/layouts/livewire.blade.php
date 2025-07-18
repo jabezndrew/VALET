f6aac4311d2df521324ac9bac26358f624c5c35f
jabezez <jabezandrew.aguspina.22@usjr.edu.ph>
2025-07-18 21:18:17 +0800

    forcing layout

--- resources/views/layouts/livewire.blade.php
+++ resources/views/layouts/livewire.blade.php
@@ -235,7 +235,7 @@
     </div>
 
     <!-- Page Content -->
-    @yield('content')
+    {{ $slot }}
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
