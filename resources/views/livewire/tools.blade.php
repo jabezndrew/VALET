09e6c9d3353ef24099bbb658888f257310bc60c5
Rextine Evangelista <evangelistarxtn@gmail.com>
2026-03-06 22:02:34 +0800

    fix

--- resources/views/livewire/tools.blade.php
+++ resources/views/livewire/tools.blade.php
@@ -192,10 +192,6 @@
             </div>
             @endif
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> rxtnnn
             <!-- Guard Display - Security, SSD, Admin -->
             @if($isSecurity || $isSsd || $isAdmin)
             <div class="col-md-4 col-lg-3">
@@ -214,11 +210,6 @@
             </div>
             @endif
 
-<<<<<<< HEAD
-=======
->>>>>>> 3a07f21c50a97e197594c00e4221fdb27f8b424d
-=======
->>>>>>> rxtnnn
             <!-- Clear Cache - Admin only -->
             @if($isAdmin)
             <div class="col-md-4 col-lg-3">
