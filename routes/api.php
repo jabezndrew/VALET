711c7b65441e49d505bd12fea4ef92ed1bba40ec
Rextine Evangelista <evangelistarxtn@gmail.com>
2026-03-22 19:58:47 +0800

    fix entry endpoint

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::prefix('public')->group(function () {
     Route::post('/rfid/exit', [RfidController::class, 'exit']);
     Route::get('/rfid/scans', [RfidController::class, 'recentScans']);
     Route::get('/rfid/tags', [RfidController::class, 'tags']);
+    Route::post('/rfid/parked', [RfidController::class, 'markParked']);
     Route::post('/guest/verify', [RfidController::class, 'verifyGuest']);
 
     // Mobile staff vehicle lookup (no side effects)
